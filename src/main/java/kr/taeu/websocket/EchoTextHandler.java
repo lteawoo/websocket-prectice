85afae74591350db747afbff95e622a546a2b910
lteawoo <lteawoo@naver.com>
2022-10-23 17:14:21 +0900

    init

---
+++ src/main/java/kr/taeu/websocket/EchoTextHandler.java
@@ -0,0 +1,57 @@
+package kr.taeu.websocket;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class EchoTextHandler extends TextWebSocketHandler {
+
+    private static List<WebSocketSession> sessionList = Collections.synchronizedList(new ArrayList<>());
+
+    /**
+     * WebSocket 연결 훅
+     * @param session
+     * @throws Exception
+     */
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessionList.add(session);
+
+        for (WebSocketSession webSocketSession : sessionList) {
+            webSocketSession.sendMessage(new TextMessage(session.getId() + " connected"));
+        }
+    }
+
+    /**
+     * WebSocket Message 수신 시 핸들링
+     * @param session
+     * @param message
+     * @throws Exception
+     */
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession webSocketSession : sessionList) {
+            webSocketSession.sendMessage(new TextMessage(session.getId() + " : " + message.getPayload()));
+        }
+    }
+
+    /**
+     * WebSocket 연결 해제 훅
+     * @param session
+     * @param status
+     * @throws Exception
+     */
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessionList.remove(session);
+
+        for (WebSocketSession webSocketSession : sessionList) {
+            webSocketSession.sendMessage(new TextMessage(session.getId() + " disconnected"));
+        }
+    }
+}
